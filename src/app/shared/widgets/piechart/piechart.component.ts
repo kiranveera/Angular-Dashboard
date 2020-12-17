a11034cff1db319d0cfaed47b5a5fad2ffdf3f31
kiranveera <52673585+kiranveera@users.noreply.github.com>
2020-12-17 18:03:16 +0530

    first commit

---
+++ src/app/shared/widgets/piechart/piechart.component.ts
@@ -0,0 +1,67 @@
+import { Component, Input, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+@Component({
+  selector: 'app-widget-piechart',
+  templateUrl: './piechart.component.html',
+  styleUrls: ['./piechart.component.scss']
+})
+export class PiechartComponent implements OnInit {
+
+  constructor() { }
+
+   chartOptions: {};
+  Highcharts = Highcharts;
+  @Input () data = [];
+  ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Random data'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        credits: {
+          enabled: false
+        },
+        exporting: {
+          enabled: true,
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: this.data
+        }]
+    };
+  HC_exporting(Highcharts);
+
+  setTimeout(()=>{
+    window.dispatchEvent(
+      new Event('reSize')
+    )
+  },3000)
+  }
+
+}
